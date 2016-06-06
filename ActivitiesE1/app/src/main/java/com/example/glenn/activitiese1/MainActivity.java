83f780a22b423855d2c720a6a7310482f6140179
Glenn Van Mele <glennvanmele@gmail.com>
2016-06-06 16:40:04 +0200

    Working exercise 1

--- ActivitiesE1/app/src/main/java/com/example/glenn/activitiese1/MainActivity.java
+++ ActivitiesE1/app/src/main/java/com/example/glenn/activitiese1/MainActivity.java
@@ -5,11 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int onCreate = 0;
+    private int onStart = 0;
+    private int onResume = 0;
+    private int onPause = 0;
+    private int onStop = 0;
+    private int onDestroy = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        onCreate++;
+        updateTextView();
     }
 
     @Override
@@ -49,4 +60,47 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    protected void onStart() {
+        onStart++;
+        updateTextView();
+        super.onStart();
+        // The activity is about to become visible.
+    }
+    @Override
+    protected void onResume() {
+        onResume++;
+        updateTextView();
+        super.onResume();
+        // The activity has become visible (it is now "resumed").
+    }
+    @Override
+    protected void onPause() {
+        onPause++;
+        updateTextView();
+        super.onPause();
+        // Another activity is taking focus (this activity is about to be "paused").
+    }
+    @Override
+    protected void onStop() {
+        onStop++;
+        updateTextView();
+        super.onStop();
+        // The activity is no longer visible (it is now "stopped")
+    }
+    @Override
+    protected void onDestroy() {
+        onDestroy++;
+        updateTextView();
+        super.onDestroy();
+        // The activity is about to be destroyed.
+    }
+
+    protected void updateTextView() {
+        setContentView(R.layout.activity_main);
+        TextView myTextView = (TextView) findViewById(R.id.counter);
+        myTextView.setText("onCreate(): " + onCreate + "\nonStart(): " + onStart + "\nonResume(): " + onResume + "\nonPause(): " + onPause + "\nonStop(): " + onStop + "\nonDestroy(): " + onDestroy);
+    }
+
 }
